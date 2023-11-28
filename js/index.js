a49a33d599170fb00eba18ad60f05c9d78ac9512
ignaciogarcia <garciaignaciouni@gmail.com>
2023-11-28 03:36:59 -0300

    se modifica el javascript completo, se agrega manejo de DOM, se comenta buena parte del HTML

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
+/* CODIGO REUTILIZADO DE LA ENTREGA 1
 //REGISTROS PRESUPUESTOS AMIGOS
 //Totales
 
-let totalGastado = 0;
+let totalGastado;
 let gastoMaximoCategoria;
 let cantidadAmigos = parseInt(prompt("¿Cuantos amigos pensas añadir a la cuenta?"));
 let dineroDisponible = parseFloat(prompt("Cuanto dinero disponible tienen?"));
@@ -10,7 +11,6 @@ let totalXAmigo;
 
 
 let gastosPorCategoria = {};
-
 // Funcion para ingreso de Datos
 function ingresarDatosAmigo() {
     let nombre = prompt("¿Cuál es el nombre de tu amigo a ingresar?");
@@ -122,10 +122,7 @@ const DOLARES = 925;
     } else{console.log("No se desea cambiar moneda");}
 
 
-let continuar = prompt("¿desea seguir agregando datos? (Si/No)").toLowerCase();
-if (continuar != "si" && continuar != "no"){
-    continuar = prompt("ingrese un dato valido (Si/No)").toLowerCase();
-}
+
 while(continuar == "si"){
     cantidadAmigos = parseInt(prompt("¿Cuantos amigos pensas añadir a la cuenta?"));
     dineroDisponible = parseFloat(prompt("Cuanto dinero disponible tienen?"));
@@ -134,16 +131,301 @@ while(continuar == "si"){
     }
     mostrarTotales();
     continuar = prompt("¿desea seguir agregando datos? (Si/No)").toLowerCase();
+}*/
+
+
+
+
+
+
+
+//DESAFIO 2
+function nuevaEtiqueta(nombreEtiqueta, etiqueta, clase, anidar){
+    nombreEtiqueta = document.createElement(etiqueta);
+    nombreEtiqueta.className = clase
+    anidar.appendChild(nombreEtiqueta);
+    return nombreEtiqueta;
+    }; //funcion para crear etiquetas HTML
+
+let totalGastado = 0;
+let contadorAmigos = 0;
+let totalGastoAmigo = 0;
+let categoriasGasto = {};
+let limite = prompt("cuanto dinero limite piensan llegar a gastar entre todos?");
+let saberLimite;
+let textoSaberLimite;
+let saberLimiteImg;
+let colorTextoLimite;
+
+let dineroDisponibleInput = prompt("Dinero Disponible");
+let dineroDisponible = parseFloat(dineroDisponibleInput);
+while (isNaN(dineroDisponible)) {
+    alert("Por favor, ingresa un valor numérico para el dinero disponible.");
+    dineroDisponibleInput = prompt("Dinero Disponible");
+    dineroDisponible = parseFloat(dineroDisponibleInput);
 }
+dineroDisponible = Math.round(dineroDisponible);
+console.log("dinero disponible: "+dineroDisponible);
+
+
+function calcularEstadisticas(){
+    function calcularDineroDisponible(totalGastado, dineroDisponible){
+        dineroDisponible -= totalGastado;
+        return dineroDisponible;
+    }; // Falta hacer funcion con funciones para llamarlas a todas en una
+    function calcularTotalGastoAmigo(totalGastado, contadorAmigos){
+        totalGastoAmigo = totalGastado / contadorAmigos;
+        return totalGastoAmigo;
+    };
+    function calcularLimite(limite, totalGastado){
+        saberLimite = limite - totalGastado;
+        console.log(saberLimite);
+        switch(true){
+            case saberLimite > 0:
+                saberLimiteImg = "<img src=./assets/img/limite-ok.png alt=dentro del limite permitido>";
+                textoSaberLimite ="Dinero antes de alcanzar el limite";
+                colorTextoLimite = "t-limite-ok"
+                return saberLimiteImg, textoSaberLimite, colorTextoLimite;
+            case saberLimite < 0:
+                saberLimiteImg = "<img src=./assets/img/limite-no.png alt=fuera del limite permitido>";
+                textoSaberLimite ="Limite pasado por";
+                colorTextoLimite = "t-limite-no"
+                return saberLimite, textoSaberLimite, colorTextoLimite;
+            case saberLimite === 0:
+                saberLimiteImg = "<img src=./assets/img/limite-ok.png alt=dentro del limite permitido>";
+                textoSaberLimite ="Estas en tu limite";
+                colorTextoLimite = "t-limite-ok"
+                saberLimite = "";
+                return saberLimiteImg, textoSaberLimite, limite, colorTextoLimite;
+            default:
+                alert("Error, ingresar dato válido");
+                saberLimite="";
+                return saberLimite;
+        }
+    }
+    calcularLimite(limite, totalGastado);
+    totalGastoAmigo = calcularTotalGastoAmigo(totalGastado, contadorAmigos);
+    if (isNaN(totalGastoAmigo)){totalGastoAmigo = 0;};
+    dineroDisponible = calcularDineroDisponible(totalGastado, dineroDisponible);
+    return totalGastoAmigo, dineroDisponible, saberLimite;
+}
+
+
+
+
+let main = document.createElement("main");
+document.body.appendChild(main);
+
+//Declaramos Section con posibles futuros graficos
+let sectionGrafico = document.createElement("section");
+sectionGrafico.className = "grafico";
+main.appendChild(sectionGrafico);
+let divTituloGrafico = document.createElement("div");
+divTituloGrafico.innerHTML = `
+                            <div class="titulo-grafico">
+                                <h1>Grafico de Gastos</h1>
+                                <img src="./assets/img/estadisticas.png" alt="icono estadisticas">
+                            </div>
+                            `;
+sectionGrafico.appendChild(divTituloGrafico);
+
+
+//Declaramos Section con todas las estadisticas del emulador
+let sectionEstadisticas = document.createElement("section");
+sectionEstadisticas.className = "estadisticas";
+main.appendChild(sectionEstadisticas);
+
+
+
+
+//Declaramos section con botones para crear o eliminar datos
+let sectionAñadirBorrarDatos = document.createElement("section");
+sectionAñadirBorrarDatos.className = "añadir-eliminar-datos";
+sectionAñadirBorrarDatos.innerHTML = `
+                                    <button type="button" class="eliminar-datos bounce-top" >Eliminar Datos</button>
+                                    <button type="button" class="agregar-datos bounce-top">Añadir Datos</button>
+                                    `
+main.appendChild(sectionAñadirBorrarDatos);
+
+
+//Se declara section con todos los datos que generara el usuario
+let sectionDatosPresupuesto = document.createElement("section");
+sectionDatosPresupuesto.className = "datos-presupuesto";
+main.appendChild(sectionDatosPresupuesto);
+
+
+//Constructor de datos
+class ingresarDatos{
+    constructor(id, amigo, gastoAmigo, categoria, fecha){
+        this.id = id,
+        this.amigo = amigo,
+        this.gastoAmigo = gastoAmigo,
+        this.categoria = categoria,
+        this.fecha = fecha
+    }
+}
+
+sectionDatosPresupuesto.innerHTML = `
+                                    <div class="cant-amigos">
+                                        <img class="d-p-titulos" src="./assets/img/cant-amigos.png" alt="doble tilde simbolico">
+                                        <div id="datos1" class="datos">
+                                        </div>
+                                    </div>
+                                    <div class="nombre-amigo">
+                                        <h2 class="d-p-titulos">Amigo</h2>
+                                        <div id="datos2" class="datos">
+                                        </div>
+                                    </div>
+                                    <div class="gasto-amigo">
+                                        <h2 class="d-p-titulos">Gastado</h2>
+                                        <div id="datos3" class="datos">
+                                        </div>
+                                    </div>
+                                    <div class="categoria-amigo">
+                                        <h2 class="d-p-titulos">Categoria</h2>
+                                        <div id="datos4" class="datos">
+                                        </div>
+                                    </div>
+                                    <div class="fecha-amigo">
+                                        <h2 class="d-p-titulos">Fecha</h2>
+                                        <div id="datos5" class="datos">
+                                        </div>
+                                    </div>
+                                    `;
+
+
+
+let continuar = prompt("¿desea añadir datos? (Si/No)").toLowerCase();
+if (continuar != "si" && continuar != "no"){continuar = prompt("ingrese un dato valido (Si/No)").toLowerCase();}
+while (continuar === "si") {
+    contadorAmigos++;
+    let nuevoDato = new ingresarDatos(
+        contadorAmigos,
+        prompt("Nombre del amigo:"),
+        parseInt(prompt("Gasto del amigo:")),
+        prompt("Categoría del gasto:"),
+        prompt("Fecha del gasto:")
+    );
+
+    let datos1 = document.getElementById("datos1");
+    let datos2 = document.getElementById("datos2");
+    let datos3 = document.getElementById("datos3");
+    let datos4 = document.getElementById("datos4");
+    let datos5 = document.getElementById("datos5");
+    datos1.innerHTML += `<p>${nuevoDato.id}</p>`;
+    datos2.innerHTML += `<p>${nuevoDato.amigo}</p>`;
+    datos3.innerHTML += `<p>${nuevoDato.gastoAmigo}</p>`;
+    datos4.innerHTML += `<p>${nuevoDato.categoria}</p>`;
+    datos5.innerHTML += `<p>${nuevoDato.fecha}</p>`;
+
+    //Registro de categorias existentes para luego sacar en cual se gasto mas
+    categoriasGasto[nuevoDato.categoria] = (categoriasGasto[nuevoDato.categoria] || 0) + nuevoDato.gastoAmigo;
+    totalGastado = totalGastado + Math.round(parseFloat(nuevoDato.gastoAmigo));
+    console.log("total gastado: "+totalGastado);
+    console.log(nuevoDato);
+    continuar = prompt("¿Quieres seguir agregando datos de amigos? si/no").toLocaleLowerCase();
+    if (continuar != "si" && continuar != "no"){continuar = prompt("ingrese un dato valido (Si/No)").toLowerCase();}
+}
+
+// Buscar de las categorias ingresadas por el user, en cual se gasto mas para luego poner en estadisticas
+let categoriaMaxGasto = "";
+let maxGasto = 0;
+for (const categoria in categoriasGasto) {
+    if (categoriasGasto[categoria] > maxGasto) {
+        maxGasto = categoriasGasto[categoria];
+        categoriaMaxGasto = categoria;
+    }
+}
+console.log("Categoría en la que se gastó más:", categoriaMaxGasto);
+console.log("Total gastado en esa categoría:", maxGasto);
+
+
+
+
+
+
+
+
+
+//declaramos las estadisticas totales despues de ingrasar todos los datos
+
+class estadistica{
+    constructor(titulo, estadistica, clase, img){
+        this.titulo = titulo,
+        this.estadistica = estadistica,
+        this.clase = clase,
+        this.img = img
+    }
+}
+
+calcularEstadisticas();
+let mostrarTotalGastado = new estadistica("Total Gastado", "$"+totalGastado,"t-gastado", "");
+let mostrarGastoCategoria = new estadistica("Gasto Maximo en Categoria", categoriaMaxGasto, "t-categoria", "");
+let mostrarDineroDisponible = new estadistica("Dinero Disponible", "$"+dineroDisponible, "t-disponible", "");
+let mostrarCantidadAmigos = new estadistica("Cant. Amigos", contadorAmigos, "t-amigos", "");
+let mostrarLimite = new estadistica("Limite a gastar en total", "$"+limite, colorTextoLimite, saberLimiteImg);
+let mostrarLimiteDisponible = new estadistica(textoSaberLimite, "$"+saberLimite, "t-limite", "")
+let mostrarTotalGastoAmigo = new estadistica("Total a gastar x Amigo", "$"+totalGastoAmigo, "t-gastar-amigo", "");
+
+
+
+const ARRAY_ESTADISTICAS = [mostrarTotalGastado, mostrarGastoCategoria, mostrarDineroDisponible, mostrarCantidadAmigos, mostrarLimite, mostrarLimiteDisponible, mostrarTotalGastoAmigo];
+
+    ARRAY_ESTADISTICAS.forEach(estadistica =>{
+    let divCards = document.createElement("div");
+    divCards.className = "e-rectangulo";
+    divCards.innerHTML = `
+                        <h2>${estadistica.titulo}</h2>
+                        <div class="estadisticas-p-img">
+                            ${estadistica.img}
+                            <p class="${estadistica.clase}">${estadistica.estadistica}</p>
+                        </div>
+                        `;
+    sectionEstadisticas.appendChild(divCards);
+    })
+
+
+
+
+
+
+
+
 
 
 //Inicio de Sesion y Registro
 
-//registro
-class usuario {
-    constructor(gmail, contraseña, repetirContraseña) {
-    this.gmail,
-    this.contraseña,
-    this.repetirContraseña
+
+class iniciarSesion {
+    constructor(nombre, apellido, gmail, contraseña){
+        this.nombre = nombre,
+        this.apellido = apellido,
+        this.gmail = gmail,
+        this.contraseña = contraseña
     }
 }
+
+
+
+
+class registrarse {
+    constructor(nuevoNombre, nuevoApellido, nuevoGmail, nuevaContraseña, repetirNuevaContraseña) {
+    this.nuevoNombre = nuevoNombre,
+    this.nuevoApellido = nuevoApellido,
+    this.nuevoGmail = nuevoGmail,
+    this.nuevaContraseña = nuevaContraseña,
+    this.repetirNuevaContraseña = repetirNuevaContraseña
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
